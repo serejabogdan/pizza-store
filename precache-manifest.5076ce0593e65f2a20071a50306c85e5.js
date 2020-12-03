94b1d250e0c92ad8ce41fe2b57c47c76d36ea80a
Hasst <sergey1112345@gmail.com>
2020-12-03 11:42:11 +0200

    Updates

---
+++ precache-manifest.5076ce0593e65f2a20071a50306c85e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5548e701f2cb59c780e6cac59acbe7b2",
+    "url": "/pizza-store/index.html"
+  },
+  {
+    "revision": "9c2bf6c3c9748788ca70",
+    "url": "/pizza-store/static/css/main.29130f21.chunk.css"
+  },
+  {
+    "revision": "837ef91d845002495f0c",
+    "url": "/pizza-store/static/js/2.e992d1fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pizza-store/static/js/2.e992d1fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2bf6c3c9748788ca70",
+    "url": "/pizza-store/static/js/main.c1a3ba92.chunk.js"
+  },
+  {
+    "revision": "12aaeae0570b92fed350",
+    "url": "/pizza-store/static/js/runtime-main.49880df7.js"
+  },
+  {
+    "revision": "2bc25b1b619d39c281d31d301207f28c",
+    "url": "/pizza-store/static/media/pizza-128.2bc25b1b.png"
+  }
+]);
