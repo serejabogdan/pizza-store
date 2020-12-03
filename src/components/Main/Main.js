aa0e82aa786374d686bdcf26bf7971ca00a20cfb
Hasst <sergey1112345@gmail.com>
2020-12-03 12:31:47 +0200

    test fix: routing

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,34 +1,28 @@
 import React from 'react';
 import './Main.scss';
 import Products from './Products/Products';
-import {Route, Switch, Redirect} from 'react-router-dom';
+import {Route, Switch, Redirect, useRouteMatch} from 'react-router-dom';
 import Cart from './Cart/Cart';
 
 function generateRoutes(paths) {
-    return paths.map(path => (
-            <Route
-                key={path}
-                exact
-                path={path}
-                component={Products}
-            />
-        )
-    );
+  return paths.map((path) => <Route key={path} exact path={path} component={Products} />);
 }
 
 const Main = (props) => {
-    const paths = ["/pizzas", "/salads", "/drinks"];
-    return (
-        <main className="Main">
-            <div className="container">
-                <Switch>
-                    {generateRoutes(paths)}
-                    <Route exact path={'/cart'} component={Cart} />
-                    <Redirect to="/pizzas" />
-                </Switch>
-            </div>
-        </main>
-    );
-}
+  let match = useRouteMatch();
+  console.log(match);
+  const paths = [`${match.url}pizzas`, `${match.url}salads`, `${match.url}drinks`];
+  return (
+    <main className="Main">
+      <div className="container">
+        <Switch>
+          {generateRoutes(paths)}
+          <Route exact path={`${match.url}cart`} component={Cart} />
+          <Redirect to={`${match.url}pizzas`} />
+        </Switch>
+      </div>
+    </main>
+  );
+};
 
 export default Main;
